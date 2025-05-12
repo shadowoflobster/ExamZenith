61024ae1153addbc04262db49a9f3c0d4b2b2072
shadowoflobster <guraspishvilileqso@gmail.com>
2025-05-12 21:06:30 +0400

    test: add unit test for getQuestion method in QuestionService

--- src/test/java/com/ExamZenith/ExamZenith/QuestionServiceTest.java
+++ src/test/java/com/ExamZenith/ExamZenith/QuestionServiceTest.java
@@ -17,6 +17,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 import java.util.Set;
 
 import static org.mockito.Mockito.when;
@@ -33,7 +34,7 @@ public class QuestionServiceTest {
     QuestionService questionService;
 
     @Test
-    void mapQuestion_shouldReturnQuestionDTO(){
+    void mapQuestion_shouldReturnCorrectQuestionDTO(){
         //Given
         Long questionId = 1L;
         String questionText = "What are four basic OOP principles?";
@@ -83,4 +84,58 @@ public class QuestionServiceTest {
         Assertions.assertTrue(questionDTO.getAnswerOptionDTOSet().stream().anyMatch(dto -> dto.getId()==102L));
 
     }
+
+    @Test
+    void getQuestion_shouldReturnQuestionDTO(){
+        //Given
+        Long questionId = 1L;
+        String questionText = "What are four basic OOP principles?";
+        QuestionType questionType = QuestionType.RADIO;
+
+        Question question = new Question();
+
+        question.setId(1L);
+        question.setQuestion_text(questionText);
+        question.setQuestion_type(questionType);
+
+        QuestionForm questionForm = new QuestionForm();
+        questionForm.setId(201L);
+
+        question.setQuestionForm(questionForm);
+
+        AnswerOption answerOption1 = new AnswerOption();
+        answerOption1.setId(101L);
+
+        AnswerOption answerOption2 = new AnswerOption();
+        answerOption2.setId(102L);
+
+
+
+        Set<AnswerOption> answerOptions = Set.of(answerOption1,answerOption2);
+        question.setOptions(answerOptions);
+
+        AnswerOptionDTO answerOption1DTO = new AnswerOptionDTO();
+        answerOption1DTO.setId(101L);
+
+        AnswerOptionDTO answerOption2DTO = new AnswerOptionDTO();
+        answerOption2DTO.setId(102L);
+
+
+        when(questionRepository.findById(questionId)).thenReturn(Optional.of(question));
+        when(answerOptionService.mapAnswerOption(answerOption1)).thenReturn(answerOption1DTO);
+        when(answerOptionService.mapAnswerOption(answerOption2)).thenReturn(answerOption2DTO);
+
+        //When
+        QuestionDTO questionDTO = questionService.getQuestion(questionId);
+
+        //Then
+        Assertions.assertEquals(1L,questionDTO.getId());
+        Assertions.assertEquals("What are four basic OOP principles?",questionDTO.getQuestion_text());
+        Assertions.assertEquals(QuestionType.RADIO,questionDTO.getQuestion_type());
+        Assertions.assertEquals(201L,questionDTO.getQuestion_form_id());
+        Assertions.assertTrue(questionDTO.getAnswerOptionDTOSet().stream().anyMatch(dto -> dto.getId()==101L));
+        Assertions.assertTrue(questionDTO.getAnswerOptionDTOSet().stream().anyMatch(dto -> dto.getId()==102L));
+
+    }
+
 }
