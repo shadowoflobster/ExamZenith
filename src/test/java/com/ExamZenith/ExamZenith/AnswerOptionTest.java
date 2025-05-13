91162b9786fdcdc7cd7b4bdd25471920a9af3dd2
shadowoflobster <guraspishvilileqso@gmail.com>
2025-05-13 13:49:05 +0400

    test: add test for getAnswerOptionById method in AnswerOptionService

--- src/test/java/com/ExamZenith/ExamZenith/AnswerOptionTest.java
+++ src/test/java/com/ExamZenith/ExamZenith/AnswerOptionTest.java
@@ -12,6 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class AnswerOptionTest {
     @Mock
@@ -37,6 +41,31 @@ public class AnswerOptionTest {
 
         //When
         AnswerOptionDTO answerOptionDTO = answerOptionService.mapAnswerOption(answerOption);
+
+        //Then
+        Assertions.assertEquals(optionId, answerOptionDTO.getId());
+        Assertions.assertEquals(optionText, answerOptionDTO.getOption_text());
+        Assertions.assertEquals(question.getId(), answerOptionDTO.getQuestion_id());
+    }
+
+    @Test
+    void getAnswerOptionById_shouldReturnAnswerOptionDTO(){
+        //Given
+        AnswerOption answerOption = new AnswerOption();
+        Long optionId = 1L;
+        String optionText = "Polymorphism";
+
+        answerOption.setId(optionId);
+        answerOption.setOption_text(optionText);
+
+        Question question = new Question();
+        question.setId(101L);
+
+        answerOption.setQuestion(question);
+
+        when(answerOptionRepository.findById(optionId)).thenReturn(Optional.of(answerOption));
+        //When
+        AnswerOptionDTO answerOptionDTO = answerOptionService.getAnswerOptionById(optionId);
 
         //Then
         Assertions.assertEquals(optionId, answerOptionDTO.getId());
