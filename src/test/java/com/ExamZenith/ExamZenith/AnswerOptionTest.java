12223b34cae9c951bace76ed2da5b47f4e5460df
shadowoflobster <guraspishvilileqso@gmail.com>
2025-05-13 13:39:40 +0400

    test: add unit test for mapAnswerOption method in AnswerOptionService

---
+++ src/test/java/com/ExamZenith/ExamZenith/AnswerOptionTest.java
@@ -0,0 +1,46 @@
+package com.ExamZenith.ExamZenith;
+
+import com.ExamZenith.ExamZenith.courses.model.AnswerOption.AnswerOptionDTO;
+import com.ExamZenith.ExamZenith.courses.persistence.AnswerOption.AnswerOption;
+import com.ExamZenith.ExamZenith.courses.persistence.AnswerOption.AnswerOptionRepository;
+import com.ExamZenith.ExamZenith.courses.persistence.Question.Question;
+import com.ExamZenith.ExamZenith.courses.service.AnswerOptionService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AnswerOptionTest {
+    @Mock
+    AnswerOptionRepository answerOptionRepository;
+
+    @InjectMocks
+    AnswerOptionService answerOptionService;
+
+    @Test
+    void mapAnswerOption_shouldMapEntityAndReturnDTO(){
+        //Given
+        AnswerOption answerOption = new AnswerOption();
+        Long optionId = 1L;
+        String optionText = "Polymorphism";
+
+        answerOption.setId(optionId);
+        answerOption.setOption_text(optionText);
+
+        Question question = new Question();
+        question.setId(101L);
+
+        answerOption.setQuestion(question);
+
+        //When
+        AnswerOptionDTO answerOptionDTO = answerOptionService.mapAnswerOption(answerOption);
+
+        //Then
+        Assertions.assertEquals(optionId, answerOptionDTO.getId());
+        Assertions.assertEquals(optionText, answerOptionDTO.getOption_text());
+        Assertions.assertEquals(question.getId(), answerOptionDTO.getQuestion_id());
+    }
+}
