0977a0f7ecc67edab02e0877fa98d897daf76d01
shadowoflobster <guraspishvilileqso@gmail.com>
2025-05-26 17:34:11 +0400

    ADD: UserQuestionAnswersRepository Added

---
+++ src/main/java/com/ExamZenith/ExamZenith/courses/persistence/UserQuestionAnswers/UserQuestionAnswersRepository.java
@@ -0,0 +1,9 @@
+package com.ExamZenith.ExamZenith.courses.persistence.UserQuestionAnswers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserQuestionAnswersRepository extends JpaRepository<UserQuestionAnswers, UserQuestionAnswersKey> {
+
+}
